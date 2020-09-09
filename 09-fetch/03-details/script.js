7f7f8a267b00042142bed4c683cbc9f48c9440b4
Dwaynebok <dwaynebok19@gmail.com>
2020-09-09 11:54:42 +0200

    finally found a way to make the code work with Awet's help

--- 09-fetch/03-details/script.js
+++ 09-fetch/03-details/script.js
@@ -9,38 +9,34 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-
-    let run = document.getElementById("run");
-    run.addEventListener("click",function (){
-
-
+(()=>{
+    function appendData(data) {
+        const target = document.getElementById("target");
+        let userInput = document.getElementById("hero-id").value;
         fetch("http://localhost:3000/heroes")
-            .then(response => response .json())
-            .then(data => appendData(data));
-
-        function appendData(data){
-            let target = document.getElementById("target");
-            document.body.appendChild(target.content.cloneNode(true));
-            for (i < 0; i >data.length;i++){
-               let tempVar = document.getElementById("tpl-hero").content.cloneNode(true);
-               tempVar.querySelector(".name").innerHTML = data[i].name;
-                tempVar.querySelector(".alter-ego").innerHTML = data[i].alterEgo;
-                tempVar.querySelector(".powers").innerHTML = data[i].abilities;
-                target.appendChild(tempVar);
-
-
-            }
-        }
+            .then(response => {
+                return response.json();
+            }).then(allData => {
+             allData.forEach(data => {
+                 if (data.id == userInput) {
+                     let tempVar = document.querySelector("#tpl-hero").content.cloneNode(true);
+                     tempVar.querySelector(".name").innerHTML = data.name;
+                     tempVar.querySelector(".alter-ego").innerHTML = data.alterEgo;
+                     tempVar.querySelector(".powers").innerHTML = data.abilities;
+                     target.appendChild(tempVar);
+                 }
+             })
 
 
+            })
 
-    })
-
-
-
-
-
-
+    }
+    let run = document.getElementById("run");
+    run.addEventListener("click", appendData);
 
 })();
+
+
+
+
+
